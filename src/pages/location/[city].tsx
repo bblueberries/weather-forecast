50b24f84b9627bf4c1833e45d4c68cac991b2034
bblueberries <tata10433@gmail.com>
2023-08-07 23:21:04 +0700

    feat: get thisCity from router

--- src/pages/location/[city].tsx
+++ src/pages/location/[city].tsx
@@ -1,7 +1,22 @@
-import React, { useEffect } from "react";
-
-type Props = {};
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { City } from "../../components/SearchBar";
 
 export default function page() {
-  return <div>this is city page</div>;
+  const router = useRouter();
+  const cities: City[] = require("../../lib/city.list.json");
+  const [thisCity, setThisCity] = useState<City | null>(null);
+
+  useEffect(() => {
+    const { city } = router.query;
+    if (!city) return;
+    if (typeof city !== "string") return;
+
+    const id = city.split("-").pop() || "";
+    const foundCity = cities.find((city) => city.id.toString() == id);
+
+    if (foundCity) setThisCity(foundCity);
+  }, [router]);
+
+  return <div>This is {thisCity?.name} the page</div>;
 }
