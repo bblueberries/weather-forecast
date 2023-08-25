525824401c87e5a32027c9db2a1898926e986b3f
bblueberries <tata10433@gmail.com>
2023-08-25 21:49:31 +0700

    feat: for mobile

--- src/components/HourlyWeather.tsx
+++ src/components/HourlyWeather.tsx
@@ -21,22 +21,26 @@ export default function HourlyWeather({
 }: Props) {
   return (
     <>
-      <p className="mt-32 font-bold text-xl">HOURLY WEATHER</p>
-      <div className="flex border border-black overflow-x-auto whitespace-nowrap mt-2 mx-20 p-6 w-7/12 rounded">
+      <p className=" mt-20 xl:mt-32 font-bold text-xl">Hourly Weather</p>
+      <div className="flex border border-black overflow-x-auto whitespace-nowrap mt-2 mx-20 p-6 w-11/12 rounded">
         {todayWeather.hour
           .slice(currentHourIndex)
           .map((hourData: HourData, index: number) => (
             <div
               key={index}
-              className="border-2 border-gray-300 mx-3 py-4 px-7 rounded flex flex-col items-center "
+              className="border-2 border-gray-300 mx-3 py-4 px-7 rounded flex flex-col items-center bg-white text-sm    "
             >
-              <p className="font-bold mb-2">{hourData.time.split(" ").pop()}</p>
+              <p className="font-semibold xl:font-bold mb-2 xl:text-base">
+                {hourData.time.split(" ").pop()}
+              </p>
               <img
                 src={`https:${hourData.condition.icon}`}
                 alt="Weather icon"
                 loading="lazy"
               />
-              <p className="text-sm mt-2">{hourData.temp_c}°C</p>
+              <p className="text-sm font-light xl:font-normal mt-2">
+                {hourData.temp_c}°C
+              </p>
             </div>
           ))}
       </div>
