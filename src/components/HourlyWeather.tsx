bfe6cc8f93cc2bb0be8412cc2734c516eb994503
bblueberries <tata10433@gmail.com>
2023-08-25 22:02:51 +0700

    fix: adjust padding

--- src/components/HourlyWeather.tsx
+++ src/components/HourlyWeather.tsx
@@ -22,13 +22,13 @@ export default function HourlyWeather({
   return (
     <>
       <p className=" mt-20 xl:mt-32 font-bold text-xl">Hourly Weather</p>
-      <div className="flex border border-black overflow-x-auto whitespace-nowrap mt-2 mx-20 p-6 w-11/12 rounded">
+      <div className="flex border border-black overflow-x-auto whitespace-nowrap mt-2 mx-20 p-3 xl:p-6 w-11/12 rounded">
         {todayWeather.hour
           .slice(currentHourIndex)
           .map((hourData: HourData, index: number) => (
             <div
               key={index}
-              className="border-2 border-gray-300 mx-3 py-4 px-7 rounded flex flex-col items-center bg-white text-sm    "
+              className="border-2 border-gray-300 mx-2 xl:mx-3 py-4 px-6 rounded flex flex-col items-center bg-white text-sm    "
             >
               <p className="font-semibold xl:font-bold mb-2 xl:text-base">
                 {hourData.time.split(" ").pop()}
